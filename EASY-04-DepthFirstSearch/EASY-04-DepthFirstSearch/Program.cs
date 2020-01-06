55481af7d677148def34d02f8865b8a97fef245e
UTECHPIA <57503203+UTECHPIA@users.noreply.github.com>
2020-01-05 23:52:58 -0800

    Added Question 4

---
+++ EASY-04-DepthFirstSearch/EASY-04-DepthFirstSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿/* You are given a Node class that has a name and an array of optional 
+ * children Nodes. When put together, Nodes form a simple tree-like 
+ * structure. Implement the depthFirstSearch method on the Node class, 
+ * which takes in an empty array, traverses the tree using the Depth-first 
+ * Search approach (specifically navigating the tree from left to right), 
+ * stores all the of the Nodes' names in the input array, and returns it.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EASY_04_DepthFirstSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
