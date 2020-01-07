2f45704c52a114245548e7975b6668957e9043d8
UTECHPIA <57503203+UTECHPIA@users.noreply.github.com>
2020-01-06 22:52:04 -0800

    Added Problem 05

---
+++ EASY-05-LinkedListConstruction/EASY-05-LinkedListConstruction/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ Write a class for a Doubly Linked List. The class should have a "head" 
+ and "tail" properties, both of which should point to either the None 
+ (null) value or to a Linked List node. Every node will have a "value" 
+ property as well as "next" and "prev" properties, both of which can 
+ point to either the None (null) value or another node. The class 
+ should support setting the head and tail of the linked list, 
+ inserting nodes before and after other nodes as well as at certain 
+ positions, removing given nodes and removing nodes with specific 
+ values, and searching for nodes with values. Only the searching 
+ method should return a value: specifically, a boolean. 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EASY_05_LinkedListConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
