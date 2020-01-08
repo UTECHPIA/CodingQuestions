4129919e3832ac4498c634321139593567823d6c
UTECHPIA <57503203+UTECHPIA@users.noreply.github.com>
2020-01-07 22:13:24 -0800

    Added 6th Question

---
+++ EASY-06-NthFibonacci/EASY-06-NthFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ The Fibonacci sequence is defined as follows: the first number 
+ of the sequence is 0, the second number is 1, and the nth number 
+ is the sum of the (n - 1)th and (n - 2)th numbers. Write a 
+ function that takes in an integer n and returns the nth Fibonacci 
+ number.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EASY_06_NthFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
