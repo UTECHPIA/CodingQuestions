0ac739774ddaed2d87a7cde82d8bca1a77bf1974
UTECHPIA <57503203+UTECHPIA@users.noreply.github.com>
2020-01-10 00:00:12 -0800

    Added Problem 08

---
+++ EASY-08-BinarySearch/EASY-08-BinarySearch/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Write a function that takes in a sorted array of 
+ integers as well as a target integer. The function 
+ should use the Binary Search algorithm to find if 
+ the target number is contained in the array and 
+ should return its index if it is, otherwise -1.
+ 
+
+ Sample input: [0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33
+
+ Sample output: 3
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EASY_08_BinarySearch
+{
+    class Program
+ 
+    
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
